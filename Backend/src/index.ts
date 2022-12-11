452702e73008767c4aa8dcb61044dee4b91bed1f
will <willamssouza917@gmail.com>
2022-12-11 20:46:20 -0300

    setting up the api, add /decks endpoint

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,13 +1,27 @@
-import express, { Request, Response } from 'express'
+import express, { Request, Response } from 'express';
+import mongoose from 'mongoose';
 
-const app = express()
+import Deck from './models/deck';
 
 const port = 8001
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello world!')
+const app = express()
+
+app.use(express.json());
+
+app.post('/decks', async (req: Request, res: Response) => {
+  const newDeck = new Deck({
+    title: req.body.title,
+  })
+
+  const created = await newDeck.save();
+  res.json(created);
 });
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`)
-});
+mongoose.set('strictQuery', true);
+mongoose.connect('mongodb+srv://mernstack:TqzacvNdbtmWDiMV@mern-stack-web-applicat.nmhumqa.mongodb.net/?retryWrites=true&w=majority').then(() => {
+  app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`)
+  });
+})
+
